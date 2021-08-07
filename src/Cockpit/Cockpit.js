214f26ff30b7cde8ad538e71f0e5c815dc6281f9
saiful-dev <saiful.hstu.sii@gmail.com>
2021-08-07 23:56:15 +0600

    Component Structure Added

---
+++ src/Cockpit/Cockpit.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Cockpit.css';
+
+const cockpit=(props)=>{
+
+    //const classes=['red','bold'].join(' ');
+    // we can write also 'red bold' in className
+   const classes=[];
+   if(props.personCoc.length <=2){
+        classes.push('red'); //classes=['red']
+   }
+   if(props.personCoc.length <=1){
+     classes.push('bold'); //classes=['red','bold']
+
+  }
+
+    return (
+        <div>
+        <h1> Hello dear</h1>
+          <p className={classes.join(' ')}>React APP!</p> {/* we need to join bcz classes takes string */}
+           <button 
+              style={props.stylejs}
+              onClick={props.toggle}>Toggle Persons</button>
+              
+              {/* when needs to updates anythings,(means we click toggle button) then render method will 
+              execute and not only return  */} 
+        </div>
+    );
+};
+
+export default cockpit;
